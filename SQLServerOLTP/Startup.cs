5b3a3946b37cf5917d5acbda0022c10eadeaad8b
vikash <vikash.prasad@healthec.com>
2021-06-13 17:47:00 +0530

    SqLServerInMemory Project added.

---
+++ SQLServerOLTP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SQLServerOLTP.Startup))]
+namespace SQLServerOLTP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
